ce7a9fadaed9b2883fa2627b87b2129e6a118c43
Enrique Mingorance Cano <emingora@redhat.com>
2020-02-20 13:59:45 +0100

    service creation

---
+++ src/main/test/java/mingorance/cano/camunda/ApplicationIT.java
@@ -0,0 +1,16 @@
+package mingorance.cano.camunda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationIT {
+
+    @Test
+    public void startUpTest() {
+        // context init test
+    }
+}
